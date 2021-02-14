786a9bd4fd8800e7ec3c94a3db9765e7b648f756
Hanno Braun <hb@hannobraun.de>
2021-02-14 11:42:43 +0100

    Derive `Debug` for `Contact`

--- src/contacts.rs
+++ src/contacts.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 /// A contact
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Contact {
     /// The contact's name
     pub name: String,
@@ -26,7 +26,7 @@ pub struct Contact {
 }
 
 /// An organization that a contact is part of
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Organization {
     /// The name of the organization
     pub name: String,
@@ -38,7 +38,7 @@ pub struct Organization {
 /// An address (or equivalent concept) related to a means of communication
 ///
 /// Could be an email address, street address, phone number, etc.
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Address {
     /// The address itself
     pub value: String,
@@ -48,7 +48,7 @@ pub struct Address {
 }
 
 /// The dates of interaction last and next with the contact
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Communication {
     /// The latest interaction with the contact
     pub latest: LatestCommunication,
@@ -57,7 +57,7 @@ pub struct Communication {
 }
 
 /// The latest communication with a contact
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct LatestCommunication {
     /// The last communication to the contact
     ///
@@ -71,7 +71,7 @@ pub struct LatestCommunication {
 }
 
 /// A planned communication with a contact
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct PlannedCommunication {
     /// The date of the planned communication
     ///
