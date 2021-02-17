dc0eb46b4465ba3df286d9890370dcfff5f54b75
Hanno Braun <hb@hannobraun.de>
2021-02-17 19:38:05 +0100

    Add tasks

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ use contacts::Contact;
 pub mod contacts;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // TASK: Load all contacts from `contacts/`.
+    // TASK: Load path of `contacts/` from a configuration file.
+    // TASK: Display only those contacts for whom a planned communication date
+    //       is due.
     let contact = Contact::load("contacts/ex-ample.toml")?;
+    // TASK: Display short summary of contact. Name, maybe dates.
     println!("{:#?}", contact);
     Ok(())
 }
