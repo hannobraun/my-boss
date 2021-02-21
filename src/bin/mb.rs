40c291ba80270b7a2b0179f7a6316b1d9f2af9af
Hanno Braun <hb@hannobraun.de>
2021-02-21 10:03:54 +0100

    Add tasks

--- src/bin/mb.rs
+++ src/bin/mb.rs
@@ -4,6 +4,10 @@ use my_boss::{config::Config, contacts::Contacts};
 use time::OffsetDateTime;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // TASK: Only display contacts, if a command is passed: `mb contacts`
+    // TASK: Add `mb init` command that creates configuration file with default
+    //       values.
+
     let config = Config::load()?;
     // TASK: Use local time instead. As of this writing, this is blocked by this
     //       issue: https://github.com/time-rs/time/issues/293
