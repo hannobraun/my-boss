6376c18daf3dd323dc4753f603597874fcbe9977
Hanno Braun <hb@hannobraun.de>
2021-02-25 18:25:28 +0100

    Refactor

--- src/bin/mb.rs
+++ src/bin/mb.rs
@@ -2,7 +2,7 @@ use clap::Clap as _;
 use my_boss::{
     args::{Args, Command},
     config::Config,
-    contacts::Contacts,
+    contacts::{Contact, Contacts},
 };
 use time::OffsetDateTime;
 
@@ -27,10 +27,18 @@ fn main() -> anyhow::Result<()> {
             // TASK: Use local time instead. As of this writing, this is blocked
             //       by this issue: https://github.com/time-rs/time/issues/293
             let today = OffsetDateTime::now_utc().date();
-            for contact in contacts.due(today) {
-                println!("{}", contact.summary()?);
-            }
+            print_contacts(contacts.due(today))?;
         }
+    }
+
+    Ok(())
+}
+
+fn print_contacts<'r>(
+    contacts: impl Iterator<Item = &'r Contact>,
+) -> anyhow::Result<()> {
+    for contact in contacts {
+        println!("{}", contact.summary()?);
     }
 
     Ok(())
