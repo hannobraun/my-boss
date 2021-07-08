e904b4beba768f5e634797b12372e15dc4d9eda9
Hanno Braun <hb@hannobraun.de>
2021-07-08 12:15:37 +0000

    Improve documentation

--- src/args.rs
+++ src/args.rs
@@ -20,20 +20,28 @@ pub enum Command {
 
 #[derive(Clap)]
 pub enum ContactsCmd {
+    /// Generate a new contact
     Generate(GenerateContact),
+
+    /// List contacts. By default, only contacts where communication is due are
+    /// listed.
     List(ListContacts),
 }
 
 #[derive(Clap)]
 pub struct GenerateContact {
+    /// The name of the new contact
     pub name: String,
 
+    /// The path to the directory where the contact will be generated. If not
+    /// specified, the path will be taken from My Boss's configuration file.
     #[clap(short, long)]
     pub path: Option<PathBuf>,
 }
 
 #[derive(Clap)]
 pub struct ListContacts {
+    /// List all contacts, not just those where communication is due.
     #[clap(short, long)]
     pub all: bool,
 }
