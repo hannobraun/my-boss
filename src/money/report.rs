c0b810f1f717be8ea0d53c45e0eafe6b894da342
Hanno Braun <hb@hannobraun.de>
2021-07-22 19:12:37 +0000

    Remove budgets from report

--- src/money/report.rs
+++ src/money/report.rs
@@ -1,10 +1,9 @@
 use std::io::{self, Write as _};
 
-use indexmap::IndexSet;
 use tabwriter::TabWriter;
 use termcolor::{Ansi, Color, ColorSpec, WriteColor as _};
 
-use super::transactions::{Accounts, Amount, Transactions};
+use super::transactions::{Amount, Transactions};
 
 pub fn write_report(
     transactions: &Transactions,
@@ -13,12 +12,6 @@ pub fn write_report(
     let writer = TabWriter::new(writer);
     let mut writer = Ansi::new(writer);
 
-    let mut budgets = AccountNames::new();
-
-    for transaction in transactions {
-        budgets.collect_names(&transaction.budgets);
-    }
-
     // Write header
     writer.set_color(
         ColorSpec::new()
@@ -26,22 +19,12 @@ pub fn write_report(
             .set_intense(true)
             .set_bold(true),
     )?;
-    write!(writer, "Date\tDescription\tAmount\tBudgets")?;
-    budgets.reserve_header_space(&mut writer)?;
+    write!(writer, "Date\tDescription\tAmount")?;
     writer.reset()?;
     writeln!(writer)?;
 
-    // Write sub-header
-    writer.set_color(
-        ColorSpec::new()
-            .set_fg(Some(Color::White))
-            .set_intense(true)
-            .set_bold(true),
-    )?;
-    write!(writer, "\t\t\t")?;
-    budgets.write_header(&mut writer)?;
-    writer.reset()?;
-    writeln!(writer)?;
+    // Write empty line below header
+    writeln!(writer, "\t\t")?;
 
     // Write transactions
     for transaction in transactions {
@@ -53,7 +36,6 @@ pub fn write_report(
 
         write_amount(&transaction.amount, false, &mut writer)?;
         write!(writer, "\t")?;
-        write_amounts(&transaction.budgets, &budgets, &mut writer)?;
 
         writer.reset()?;
         writeln!(writer)?;
@@ -66,29 +48,12 @@ pub fn write_report(
             .set_intense(true)
             .set_bold(true),
     )?;
-    write!(writer, "\tTotals\t")?;
+    write!(writer, "\tTotal\t")?;
     write_amount(&transactions.total(), true, &mut writer)?;
-    write!(writer, "\t")?;
-    budgets.write_totals(&transactions, &mut writer)?;
     writer.reset()?;
     writeln!(writer)?;
 
     writer.flush()?;
-
-    Ok(())
-}
-
-fn write_amounts(
-    accounts: &Accounts,
-    names: &AccountNames,
-    writer: &mut Ansi<impl io::Write>,
-) -> anyhow::Result<()> {
-    for name in &names.0 {
-        if let Some(amount) = accounts.amount_for(name) {
-            write_amount(&amount, false, writer)?;
-        }
-        write!(writer, "\t")?;
-    }
 
     Ok(())
 }
@@ -113,52 +78,4 @@ fn write_amount(
     write!(writer, "{}", amount)?;
 
     Ok(())
-}
-
-struct AccountNames(IndexSet<String>);
-
-impl AccountNames {
-    fn new() -> Self {
-        Self(IndexSet::new())
-    }
-
-    fn collect_names(&mut self, accounts: &Accounts) {
-        for name in accounts.names() {
-            self.0.insert(name.clone());
-        }
-    }
-
-    fn reserve_header_space(
-        &self,
-        mut writer: impl io::Write,
-    ) -> anyhow::Result<()> {
-        for _ in 0..self.0.len() {
-            write!(writer, "\t")?;
-        }
-
-        Ok(())
-    }
-
-    fn write_header(&self, mut writer: impl io::Write) -> anyhow::Result<()> {
-        for name in &self.0 {
-            write!(writer, "{}\t", name)?;
-        }
-
-        Ok(())
-    }
-
-    fn write_totals(
-        &self,
-        transactions: &Transactions,
-        writer: &mut Ansi<impl io::Write>,
-    ) -> anyhow::Result<()> {
-        for name in &self.0 {
-            let amount = transactions.account_total(name);
-            write_amount(&amount, true, writer)?;
-
-            write!(writer, "\t")?;
-        }
-
-        Ok(())
-    }
 }
